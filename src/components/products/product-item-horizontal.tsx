97abf2e5a23bbdfd48ecec611145217ac8216ff9
aguskoll <aguskoll@gmail.com>
2025-03-14 16:57:42 -0300

    item horizontal

--- src/components/products/product-item-horizontal.tsx
+++ src/components/products/product-item-horizontal.tsx
@@ -1,13 +1,6 @@
 /* eslint-disable max-lines-per-function */
 import { useState } from 'react';
-import {
-  Alert,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { Alert, Image, Text, TouchableOpacity, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { type Product, useAddFav } from '@/api/products/use-products';
@@ -17,7 +10,6 @@ import {
   useRemoveFromCart,
 } from '@/api/shopping-cart/use-shopping-cart';
 import { Button } from '@/ui';
-import { black } from '@/ui/colors';
 
 import { ProductState } from './product-state';
 
@@ -68,23 +60,30 @@ export function ProductItemHorizontal({ product }: { product: Product }) {
   };
 
   return (
-    <View style={styles.itemsSeparator}>
-      <View key={product.id} style={styles.cardContainer}>
-        <Image source={{ uri: product.pictures[0] }} style={styles.image} />
-        <View style={styles.descriptionContainer}>
-          <View style={styles.descriptionColumn}>
-            <View style={{ flex: 1 }}>
-              <Text style={[styles.text, { paddingBottom: 8 }]}>
+    <View className="flex-col">
+      <View
+        className="flex h-[160px] w-full flex-row bg-white"
+        key={product.id}
+      >
+        <Image
+          source={{ uri: product.pictures[0] }}
+          className="h-full w-[100px] object-contain"
+          style={{ resizeMode: 'contain' }}
+        />
+        <View className="flex-1 flex-row content-between p-4">
+          <View className="flex-1 flex-col">
+            <View className="flex-1">
+              <Text className="pb-4 text-[16px] font-bold color-black">
                 {product.title}
               </Text>
               <ProductState state={product.state.toString()}></ProductState>
             </View>
-            <Text style={[styles.text, { paddingBottom: 16 }]}>
+            <Text className="pb-4 text-[16px] font-bold color-black">
               {product.unit_price}
             </Text>
           </View>
 
-          <View style={styles.interactionsColumn}>
+          <View className="flex-1 flex-col content-between items-end justify-between">
             <TouchableOpacity onPress={toggleFavorite}>
               <Ionicons
                 name={isFavorite ? 'heart' : 'heart-outline'}
@@ -100,53 +99,7 @@ export function ProductItemHorizontal({ product }: { product: Product }) {
           </View>
         </View>
       </View>
-      <View style={{ height: 1, width: '100%', backgroundColor: black }} />
+      <View className="h-px w-full bg-black" />
     </View>
   );
 }
-
-export const styles = StyleSheet.create({
-  cardContainer: {
-    display: 'flex',
-    width: '100%',
-    height: 160,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-  },
-  itemsSeparator: {
-    flexDirection: 'column',
-  },
-  descriptionContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    flex: 1,
-    padding: 8,
-  },
-  descriptionColumn: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  interactionsColumn: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end',
-    alignContent: 'space-between',
-  },
-  text: {
-    color: black,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  priceText: {
-    color: black,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  addCartButton: {},
-  addCartButtonLabel: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  image: { width: 100, height: '100%', resizeMode: 'contain' },
-});
