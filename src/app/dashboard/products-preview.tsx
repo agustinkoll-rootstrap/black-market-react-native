0de43e203343187775545ba91dfb1ef614e4118c
aguskoll <aguskoll@gmail.com>
2025-04-04 14:14:02 -0300

    added sales banner

--- src/app/dashboard/products-preview.tsx
+++ src/app/dashboard/products-preview.tsx
@@ -5,6 +5,8 @@ import { FlatList } from 'react-native-gesture-handler';
 import { useProducts } from '@/api/products/use-products';
 import { ProductItemVertical } from '@/components/products/product-item-vertical';
 
+import SaleItem from './sale-item';
+
 export function ProductsPreview() {
   const { data: productsData } = useProducts();
 
@@ -20,7 +22,13 @@ export function ProductsPreview() {
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => <ProductItemVertical product={item} />}
       ></FlatList>
+      <SaleItem
+        saleDescription={{
+          title: 'Check out our new and restored furniture',
+          description: 'Shop today and get a 10% discount!',
+          imagePath: 'asd',
+        }}
+      ></SaleItem>
     </View>
   );
 }
-
