cbb52d6021c9d7a415ece8ea4d8c8aee45226ddd
aguskoll <aguskoll@gmail.com>
2025-04-08 09:59:16 -0300

    refactor de la view de checkout

--- src/app/checkout.tsx
+++ src/app/checkout.tsx
@@ -1,42 +1,49 @@
 import { Alert, View } from 'react-native';
 
-import { type OrderPayload, useCheckout } from '@/api/checkout//use-checkout';
+import { useCheckout } from '@/api/checkout//use-checkout';
 
-import AddressForm, { type AddressFormProps } from './checkout/address-form';
+import AddressForm, {
+  type AddressFormProps,
+  type FormType,
+} from './checkout/address-form';
+
+function createOrderPayload(data: FormType) {
+  const {
+    city,
+    state,
+    country,
+    address,
+    secondAddress,
+    postalCode,
+    cardNumber,
+    expirationDate,
+    securityCode,
+  } = data;
+  return {
+    order: {
+      credit_card: {
+        card_number: cardNumber,
+        exp_month: parseInt(expirationDate.split('/')[0]),
+        exp_year: parseInt(expirationDate.split('/')[1]),
+        cvc: securityCode,
+      },
+      shipping_address: {
+        city,
+        state,
+        country,
+        line_1: address,
+        line_2: secondAddress ?? '',
+        postal_code: postalCode,
+      },
+    },
+  };
+}
 
 export default function Checkout() {
   const { mutate: checkoutMutation } = useCheckout();
 
   const onSubmit: AddressFormProps['onSubmit'] = (data) => {
-    const {
-      city,
-      state,
-      country,
-      address,
-      secondAddress,
-      postalCode,
-      cardNumber,
-      expirationDate,
-      securityCode,
-    } = data;
-    const orderPayload: OrderPayload = {
-      order: {
-        credit_card: {
-          card_number: cardNumber,
-          exp_month: parseInt(expirationDate.split('/')[0]),
-          exp_year: parseInt(expirationDate.split('/')[1]),
-          cvc: securityCode,
-        },
-        shipping_address: {
-          city,
-          state,
-          country,
-          line_1: address,
-          line_2: secondAddress ?? '',
-          postal_code: postalCode,
-        },
-      },
-    };
+    const orderPayload = createOrderPayload(data);
     checkoutMutation(orderPayload, {
       onSuccess: () => {
         Alert.alert('Success', 'Order placed successfully!');
